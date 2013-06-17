1491116a857bd8934228c290a074e16621501f42
Matthew Michihara <matthew.michihara@gmail.com>
2013-06-16 22:18:43 -0700

    Modify to my liking

---
+++ android/env.zsh
@@ -0,0 +1,4 @@
+# General Android stuff
+export ANDROID_HOME=$HOME/Code/sdks/android-sdk-macosx
+export ANDROID_NDK_ROOT=$HOME/Code/sdks/android-ndk-r8d
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_ROOT:$PATH
