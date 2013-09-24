a6da7617964ee412a32e7e49b7c46161812c4d91
Matthew Michihara <matthew.michihara@gmail.com>
2013-09-24 10:49:50 -0700

    Add Android SDK build-tools to path

--- android/env.zsh
+++ android/env.zsh
@@ -1,4 +1,4 @@
 # General Android stuff
 export ANDROID_HOME=$HOME/Code/sdks/android-sdk-macosx
 export ANDROID_NDK_ROOT=$HOME/Code/sdks/android-ndk-r8d
-export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_ROOT:$PATH
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_ROOT:$ANDROID_HOME/build-tools/18.0.1:$PATH
