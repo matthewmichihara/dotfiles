a21c606e1f9fddb4fb3703912e201890e5ffb045
Matthew Michihara <matthew.michihara@gmail.com>
2014-03-25 10:18:42 -0700

    Remove ssl hack

--- lookout/env.zsh
+++ lookout/env.zsh
@@ -1,6 +1,2 @@
 export FLX_TARGET=Android
 export SECURITY_CORE_PATH=$HOME/Code/lookout/security
-
-# Set to no verify so that pulling from gerrit
-# works without weird errors.
-export GIT_SSL_NO_VERIFY=true
