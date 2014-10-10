29b7c9f581456ffcb21bbd0ccb75a46dcf405cb5
Matthew Michihara <matthew.michihara@gmail.com>
2014-10-10 13:47:25 -0400

    change android sdk loc and other stuff

--- android/env.zsh
+++ android/env.zsh
@@ -1,3 +1,3 @@
 # General Android stuff
-export ANDROID_HOME=/Applications/Android\ Studio.app/sdk
+export ANDROID_HOME=~/Code/android-sdk-macosx
 export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,8 +4,8 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LS_COLORS="di=36;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
-export CLICOLOR=true
+#export LS_COLORS="di=36;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+#export CLICOLOR=true
 
 fpath=($ZSH/functions $fpath)
 
