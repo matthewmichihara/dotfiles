b8bf753f5345449d320dc31fd9901a522def0c25
Matthew Michihara <matthew.michihara@gmail.com>
2015-07-30 18:54:19 -0400

    Make history search work with tmux

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
+# Vim as the default editor
 export EDITOR='vim'
+
+# But still use emacs-style zsh bindings (without this
+# stuff like C-R for history search doesn't work for some
+# reason
+bindkey -e
