c9e0a7e505d81080ad56721bab9954a0015b75cd
Matthew Michihara <matthew.michihara@gmail.com>
2017-04-05 23:52:23 -0400

    Add gcloud env variables

---
+++ gcloud/path.zsh
@@ -0,0 +1,7 @@
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mattm/Code/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/mattm/Code/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/mattm/Code/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/mattm/Code/google-cloud-sdk/completion.zsh.inc'; fi
